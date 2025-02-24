2f9d39c0f26f7c4d2236c870218f82a3669d1826
Konrad Dziekonski <jaseveen@gmail.com>
2025-02-24 19:32:34 +0100

    feat(desktop nav): handle motionDiv placement on resize

---
+++ src/app/components/desktopNav/navAnimation/hooks/useOnResize.ts
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { AnimationControls } from "motion/react";
+import { orZero } from "@/utils/orZero";
+import { useRefsContext } from "../../context";
+
+type Props = {
+  controls: AnimationControls;
+  getMotionDivRect: () => DOMRect | undefined;
+};
+
+export const useOnResize = ({ controls, getMotionDivRect }: Props) => {
+  const pathname = usePathname();
+  const { motionDivPosition, getCurrentLinkRect } = useRefsContext();
+
+  useEffect(() => {
+    const differenceBetweenLinkAndMotionDiv =
+      motionDivPosition.current.x - orZero(getCurrentLinkRect()?.left) - orZero(getMotionDivRect()?.width) / 2;
+
+    const hanldeResize = () => {
+      const currentLinkRect = getCurrentLinkRect();
+      const beforeNavLinkClick = !motionDivPosition.current.x;
+
+      controls.set({
+        x: beforeNavLinkClick
+          ? orZero(currentLinkRect?.left)
+          : orZero(currentLinkRect?.left) + differenceBetweenLinkAndMotionDiv,
+      });
+    };
+
+    window.addEventListener("resize", hanldeResize);
+    return () => window.removeEventListener("resize", hanldeResize);
+  }, [pathname, controls, motionDivPosition, getCurrentLinkRect, getMotionDivRect]);
+};
