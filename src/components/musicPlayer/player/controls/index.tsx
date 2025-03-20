b1fff6e2d9e49f0da21bfab88bd2141f9c8e0529
Konrad Dziekonski <jaseveen@gmail.com>
2025-03-20 08:22:29 +0100

    chore(store): add store pieces

--- src/components/musicPlayer/player/controls/index.tsx
+++ src/components/musicPlayer/player/controls/index.tsx
@@ -1,11 +1,15 @@
+import { usePlayer } from "../../context";
 import { PlayButton } from "./components/playButton";
 import { NextSongIcon, VolumeIcon } from "./icons";
 
 export const Controls = () => {
+  const { playerState, togglePlay } = usePlayer();
+  const isPlaying = playerState === "playing";
+
   return (
     <div className="w-1/2 mx-auto flex justify-around">
       <VolumeIcon />
-      <PlayButton />
+      <PlayButton {...{ isPlaying, togglePlay }} />
       <NextSongIcon />
     </div>
   );
--- src/components/musicPlayer/player/index.tsx
+++ src/components/musicPlayer/player/index.tsx
@@ -1,9 +1,17 @@
+"use client";
+import { Track, PlayerStoreProvider } from "../context";
 import { Controls } from "./controls";
 
-export const Player = () => {
+type Props = {
+  tracks: Track[];
+};
+
+export const Player = ({ tracks }: Props) => {
   return (
-    <div className="bg-black rounded-md p-2 mr-auto w-full sm:w-sm flex">
-      <Controls />
-    </div>
+    <PlayerStoreProvider {...{ tracks }}>
+      <div className="bg-black rounded-md p-2 mr-auto w-full sm:w-sm flex">
+        <Controls />
+      </div>
+    </PlayerStoreProvider>
   );
 };
