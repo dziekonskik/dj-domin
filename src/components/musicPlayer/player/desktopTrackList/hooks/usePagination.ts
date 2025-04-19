4643ba4a0b161d88bd43b3db701605c52805ac56
Konrad Dziekonski <jaseveen@gmail.com>
2025-04-19 10:32:55 +0200

    feat(musicPlayer): sync pageIndex with activeTrackIndex

--- src/components/musicPlayer/player/desktopTrackList/hooks/usePagination.ts
+++ src/components/musicPlayer/player/desktopTrackList/hooks/usePagination.ts
@@ -1,12 +1,19 @@
 import { usePlayer } from "@/components/musicPlayer/context";
 import { splitEvery } from "ramda";
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { TRACKS_PER_PAGE } from "../../consts/trackConfig";
 
 export const usePagination = () => {
   const [currentPageIdx, setCurrentPageIdx] = useState(0);
-  const { tracks } = usePlayer();
+  const { tracks, activeIndex } = usePlayer();
   const paginatedTracks = splitEvery(TRACKS_PER_PAGE, tracks);
+
+  useEffect(() => {
+    setCurrentPageIdx((prevIndex) => {
+      const newPageIndex = Math.floor(activeIndex / TRACKS_PER_PAGE);
+      return prevIndex === newPageIndex ? prevIndex : newPageIndex;
+    });
+  }, [activeIndex]);
 
   const prevPage = useCallback(() => {
     const maxPages = Math.floor(tracks.length / TRACKS_PER_PAGE);
