cfee7e377e3534b58ec0d95d4a2b415b5acf72d7
Konrad Dziekonski <jaseveen@gmail.com>
2025-04-19 09:25:54 +0200

    feat(hero): add hero ui

--- src/app/components/main/hero/index.tsx
+++ src/app/components/main/hero/index.tsx
@@ -4,8 +4,8 @@ import { MusicPlayer } from "@/components/musicPlayer";
 
 export const Hero = () => {
   return (
-    <section className="w-full grid sm:grid-cols-2 container mx-auto sm:px-2 md:px-4">
-      <div className="relative w-full aspect-1/1 sm:rounded-xl sm:overflow-hidden row-start-1 col-start-1">
+    <section className="w-full h-svh sm:h-[calc(100dvh-84px)] grid lg:grid-cols-2 lg:container mx-auto md:pb-20 md:px-4">
+      <div className="relative w-full md:rounded-xl sm:overflow-hidden row-start-1 col-start-1">
         <Image
           src={djDominPath}
           alt="Dj Domin prowadzi imprezę"
@@ -14,12 +14,16 @@ export const Hero = () => {
           sizes="(max-width: 639.99px) 100vw, 50vw"
         />
       </div>
-      <div className="flex flex-col sm:items-end row-start-1 col-start-1 sm:col-start-2 z-10 pointer-events-none sm:pointer-events-auto">
-        <h1 className="font-grotezk text-6xl sm:text-5xl md:text-6xl lg:text-8xl xl:text-9xl mt-10 xl:mt-12 uppercase ml-2">
-          DJ <span className="sm:bg-prmiary rounded-lg inline-block sm:pr-8 sm:pt-6">DOMIN</span>
-        </h1>
-        <h2 className="uppercase text-2xl xl:text-5xl 2xl:text-6xl mt-4 hidden sm:block">Zamiksuję do kotleta</h2>
-        <MusicPlayer />
+      <div className="flex flex-col row-start-1 col-start-1 lg:col-start-2 z-10">
+        <div className="px-2 grid justify-center">
+          <h1 className="font-grotezk text-6xl sm:text-7xl md:text-8xl 2xl:text-9xl mt-10 md:mt-0 uppercase">
+            DJ <span className="sm:bg-prmiary rounded-lg inline-block sm:pr-8 sm:pt-6">DOMIN</span>
+          </h1>
+          <h2 className="uppercase text-4xl xl:text-5xl 2xl:text-6xl mt-4 ">Zamiksuję do kotleta</h2>
+        </div>
+        <div className="mt-auto sm:ml-auto bg-white md:w-min md:p-2 md:pb-0">
+          <MusicPlayer />
+        </div>
       </div>
     </section>
   );
