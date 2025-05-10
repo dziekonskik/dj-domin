8d4e34e050cd341ab4a836b3cef512b85b3172ca
Konrad Dziekonski <jaseveen@gmail.com>
2025-05-10 07:53:49 +0200

    feat(context): add active section to ui context

--- src/app/context/index.tsx
+++ src/app/context/index.tsx
@@ -1,11 +1,15 @@
 "use client";
-import { createContext, PropsWithChildren, use, useCallback, useRef } from "react";
+import { createContext, PropsWithChildren, use, useCallback, useRef, useState } from "react";
 import { UIState } from "./types";
+import { SectionId, SECTION_ID } from "@/consts/sections";
 
 const UiStateContext = createContext<UIState | null>(null);
 
 export const UIStateProvider = ({ children }: PropsWithChildren) => {
+  const [activeCtaId, setActiveCtaId] = useState<SectionId>(SECTION_ID.HERO);
   const bodyScrollLockRef = useRef(false);
+
+  console.log({ activeCtaId });
 
   const isBodyScrollLocked = useCallback(() => bodyScrollLockRef.current, []);
   const setBodyScrollLocked = useCallback(() => {
@@ -15,8 +19,14 @@ export const UIStateProvider = ({ children }: PropsWithChildren) => {
     bodyScrollLockRef.current = false;
   }, []);
 
+  const setActiveCta = useCallback((id: SectionId) => {
+    setActiveCtaId(id);
+  }, []);
+
   return (
-    <UiStateContext value={{ isBodyScrollLocked, setBodyScrollLocked, setBodyScrollUnlocked }}>
+    <UiStateContext
+      value={{ isBodyScrollLocked, setBodyScrollLocked, setBodyScrollUnlocked, activeCtaId, setActiveCta }}
+    >
       {children}
     </UiStateContext>
   );
--- src/app/context/types/index.ts
+++ src/app/context/types/index.ts
@@ -1,5 +1,9 @@
+import { SectionId } from "@/consts/sections";
+
 export type UIState = {
   isBodyScrollLocked: () => boolean;
   setBodyScrollLocked: () => void;
   setBodyScrollUnlocked: () => void;
+  setActiveCta: (id: SectionId) => void;
+  activeCtaId: SectionId;
 };
