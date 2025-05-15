ebc15e75042db59b62d73096b6dbcb711a1e8c76
Konrad Dziekonski <jaseveen@gmail.com>
2025-05-15 08:12:40 +0200

    feat(contactForm): add submit button

--- src/app/components/main/contact/components/contactForm/components/submitButton/index.tsx
+++ src/app/components/main/contact/components/contactForm/components/submitButton/index.tsx
@@ -1,18 +1,58 @@
 import { CircleCta } from "@/components/circleCta";
-import { useFormStatus } from "react-dom";
-import { FormState } from "../../types";
+import { AnimatePresence, motion } from "motion/react";
+import { match } from "ts-pattern";
+import { Lifecycle } from "@/types/common";
 
 type Props = {
-  formState: FormState;
+  formStatus: Lifecycle;
 };
 
-export const SubmitButton = ({ formState }: Props) => {
-  const { pending, data, method, action } = useFormStatus();
+export const SubmitButton = ({ formStatus }: Props) => {
+  const { key, text } = match(formStatus)
+    .with("pending", () => ({
+      key: "pending",
+      text: "LECI...",
+    }))
+    .with("error", () => ({
+      key: "error",
+      text: "BŁĄD :(",
+    }))
+    .with("success", () => ({
+      key: "success",
+      text: "POSZŁO! :)",
+    }))
+    .with("idle", () => ({
+      key: "default",
+      text: "WYŚLIJ",
+    }))
+    .otherwise(() => ({
+      key: "empty",
+      text: "",
+    }));
 
-  console.log({ pending, data: data?.get("name"), method, action, formState });
+  console.log({ key });
   return (
-    <CircleCta variant="button" id="contact" onClick={undefined} layoutId="contactBtn">
-      WYŚLIJ
+    <CircleCta
+      variant="button"
+      type="submit"
+      id="contact"
+      disabled={formStatus === "pending"}
+      onClick={undefined}
+      layoutId="contactBtn"
+      className="relative w-28 rounded-full aspect-square"
+    >
+      <AnimatePresence mode="wait">
+        <motion.span
+          key={key}
+          className="absolute w-full h-full rounded-full grid place-content-center"
+          initial={{ opacity: 0, y: 10, scale: 0.95 }}
+          animate={{ opacity: 1, y: 0, scale: 1 }}
+          exit={{ opacity: 0, y: -10, scale: 0.95 }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        >
+          {text}
+        </motion.span>
+      </AnimatePresence>
     </CircleCta>
   );
 };
