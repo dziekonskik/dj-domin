eebf9c34ec3400420957aed0cfd1b22b980cc6f2
Konrad Dziekonski <jaseveen@gmail.com>
2025-05-24 16:26:13 +0200

    fix(build): fix build issue

--- src/app/components/footer/components/playerWidget/index.tsx
+++ src/app/components/footer/components/playerWidget/index.tsx
@@ -1,14 +1,11 @@
 import { Suspense } from "react";
 import { PlayerLoading } from "@/components/musicPlayer/player/loading";
-import { getMusic } from "@/components/musicPlayer/utils/getMusic";
 import { Widget } from "./components/widget";
 
 export const PlayerWidget = () => {
-  const tracksPromise = getMusic();
-
   return (
     <Suspense fallback={<PlayerLoading />}>
-      <Widget {...{ tracksPromise }} />
+      <Widget />
     </Suspense>
   );
 };
